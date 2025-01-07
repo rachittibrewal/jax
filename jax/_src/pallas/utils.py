f7ea20820cec6ceee19bb8706762e24ef3efd615
rachit173 <rachittibrewal@outlook.com>
2025-01-07 21:07:41 +0000

    Remove eos.

--- jax/_src/pallas/utils.py
+++ jax/_src/pallas/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Pallas utility functions."""
+"""Pallas utility functions"""
 
 from __future__ import annotations
 from typing import overload
